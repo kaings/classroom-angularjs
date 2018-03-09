b1b5848716a41198e456414435f9f237df63d1b2
gr33ngr33n <actfun07@gmail.com>
2018-03-09 23:55:33 +0800

    thenetninja T8~T10 complete

--- app/app5.js
+++ app/app5.js
@@ -2,6 +2,37 @@ var myProj1 = angular.module('myProj1',[]);
 
 myProj1.controller('MyProj1Controller', ['$scope', function($scope){
 
-  $scope.sports = ['swimming','badminton','tennis','marathon','basketball'];
+  $scope.sports = [
+    {
+      name: 'swimming',
+      level: 2,
+      cost: 200
+    },
+
+    {
+      name: 'badminton',
+      level: 1,
+      cost: 150
+    },
+
+    {
+      name: 'tennis',
+      level: 2,
+      cost: 200
+    },
+
+    {
+      name: 'marathon',
+      level: 3,
+      cost: 300
+    },
+
+    {
+      name: 'basketball',
+      level: 4,
+      cost: 500
+    }
+
+  ];
 
 }]);
